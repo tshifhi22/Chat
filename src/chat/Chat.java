072017439564e3eb17afe1ea0cc9161cdc4816bb
tshifhi22 <ST10514588@rcconnect.edu.za>
2026-08-18 12:25:32 +0200

    Switch to console Scanner for user input
    Replace the GUI JOptionPane with java.util.Scanner and add interactive console prompts to collect name, last name, username, email, and password. Introduce validation loops that call checkUserName/checkEmail/checkPassword (password checker stub added). Remove javax.swing import. Compiled Chat.class was updated to reflect these changes.

--- src/chat/Chat.java
+++ src/chat/Chat.java
@@ -8,7 +8,7 @@ package chat;
  *
  * @author tshik
  */
-import javax.swing.JOptionPane;
+import java.util.Scanner;
 public class Chat {
     /**
      * @param args the command line arguments
@@ -16,7 +16,66 @@ public class Chat {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        JOptionPane.showMessageDialog (null, "Hello, World!");
-    }
-    
+       Scanner input = new Scanner (System.in);
+       String name;
+       String lastName;
+       String userName;
+       String email;
+       String password;
+       
+       //Request user to input name
+        System.out.println("Please enter your name");
+        name = input.nextLine();
+        
+        //Request user to input lastName
+        System.out.println("Please enter your last name");
+        lastName = input.nextLine();
+        
+        while (true) {
+            System.out.println("Please enter your username");
+            userName = input.nextLine();
+            
+            if (checkUserName(userName)){
+                System.out.println("Username successfully captured");
+                break;
+            } else {
+                System.out.println("Invalid username");
+                System.out.println("");
+            }
+        }
+        
+        //Request user to input email address
+        while(true) {
+            System.out.println("Please enter your email addrerss");
+            email = input.nextLine();
+            
+            if (checkEmail(email)) {
+                System.out.println("Email has been successfully captured.");
+                break;
+            } else { 
+                System.out.println("Invaild email address");
+                System.out.println("Email must be in the format: example@gmail.com");
+            }
+        }
+        
+        //Request user to input password
+        while (true) {  
+            System.out.println("Enter your password");
+            password = input.nextLine();
+            
+            if (checkPassword(password)) {
+                System.out.println("Password was successfully captured.");
+                break;
+            } else {
+                System.out.println ("Password is not entered coorrected.");
+                System.out.println("Password must:");
+                System.out.println("- Be at least 8 characters long");
+                System.out.println("- Contain at least one capital letter");
+                System.out.println("- Contain at  least one number");
+                System.out.println("-Contain at least one special character");
+            }
+        }
+        input.close();
+       } 
+    public static boolean checkPassword (String password) {
 }
